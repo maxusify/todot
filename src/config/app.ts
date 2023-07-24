1163f9f40eb1b1277890a739bcb0a9df468157d2
Michał Pędzik <maxusify@gmail.com>
2023-07-24 13:14:06 +0200

    Config, simple docs strings

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,1 +1,11 @@
-export const ELLIPSIS_LIMIT = 75;
+// Text handling
+export const ELLIPSIS_LIMIT: number = 75;
+
+// Forms limits
+export const FORM_TITLE_LIMIT: number = 150;
+export const FORM_DESC_LIMIT: number = 1000;
+
+// Color indicators
+export const IMPORTANT_WARNING_COLOR: string = "text-red-500";
+export const LESSER_WARNING_COLOR: string = "text-orange-400";
+export const CORRECT_COLOR: string = "text-green-400";
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,15 +1,18 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
-import { ELLIPSIS_LIMIT } from "@/config/app";
- 
+import { ELLIPSIS_LIMIT, IMPORTANT_WARNING_COLOR, LESSER_WARNING_COLOR } from "@/config/app";
+
+/// Shadcn/ui helper function
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
+/// Capitalizes first letter of a string
 export const capitalizeFirstLetter = (word: string): string =>
   `${word.charAt(0).toUpperCase()}${word.slice(1)}`;
 
+/// Appends ellipsis effect for long text.
 export const ellipsis = (text: string, limit: number = ELLIPSIS_LIMIT) => {
   if (text.length <= limit) return text;
   return `${text.slice(0, limit - 1)}...`;
