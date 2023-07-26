129ae840dbcb4d29bbcfe5a289703f5dc99af779
Michał Pędzik <maxusify@gmail.com>
2023-07-26 12:20:07 +0200

    App config for field length limits

--- src/config/app.ts
+++ src/config/app.ts
@@ -5,6 +5,10 @@ export const ELLIPSIS_LIMIT: number = 75;
 export const FORM_TITLE_LIMIT: number = 150;
 export const FORM_DESC_LIMIT: number = 1000;
 
+// Warning percentages
+export const FORM_FIELD_LENGTH_IMPORTANT_WARN: number = 0.9;
+export const FORM_FIELD_LENGTH_LESSER_WARN: number = 0.7;
+
 // Color indicators
 export const IMPORTANT_WARNING_COLOR: string = "text-red-500";
 export const LESSER_WARNING_COLOR: string = "text-orange-400";
