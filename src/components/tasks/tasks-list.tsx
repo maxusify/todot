ade68bf59cd5c8b39aa8812338ad7049fc6a3c07
Michał Pędzik <maxusify@gmail.com>
2023-07-26 12:19:10 +0200

    Loading state for data table component

--- src/components/tasks/tasks-list.tsx
+++ src/components/tasks/tasks-list.tsx
@@ -6,19 +6,11 @@ import { columns } from "./columns";
 import DataTable from "./data-table";
 
 type Props = {
-  data: Task[] | undefined;
+  data: Task[];
 };
 
 const TasksList = ({ data }: Props) => {
-  let tasksData: Task[] = [];
-
-  if (typeof data !== undefined) tasksData = data || [];
-
-  return (
-    <div className="container mx-auto py-10 px-0">
-      <DataTable columns={columns} data={tasksData} />
-    </div>
-  );
+  return <DataTable columns={columns} data={data} />;
 };
 
 export default TasksList;
--- src/components/tasks/tasks.tsx
+++ src/components/tasks/tasks.tsx
@@ -4,16 +4,22 @@ import { useLiveQuery } from "dexie-react-hooks";
 
 import { db } from "@/lib/db";
 
+import { Skeleton } from "../ui/skeleton";
+
 import TasksControls from "./tasks-controls";
 import TasksList from "./tasks-list";
 
 const Tasks = () => {
   const data = useLiveQuery(() => db.tasks.toArray());
 
+  let taskListContent = <Skeleton className="h-[300px] w-full" />;
+
+  if (data) taskListContent = <TasksList data={data} />;
+
   return (
     <>
       <TasksControls />
-      <TasksList data={data} />
+      <div className="container mx-auto py-10 px-0">{taskListContent}</div>
     </>
   );
 };
